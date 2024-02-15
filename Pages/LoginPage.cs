b29da7718180535e421a0347b3d6271737d09452
Laszlo Molnar <laszlo.imre.molnar@gmail.com>
2024-02-15 08:49:24 +0100

    Handle the popups on Login page

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -4,6 +4,8 @@
     {
         #region UI Elements
 
+        private ILocator securityPopUpNextButton => _page.Locator("css=button._pendo-button-primaryButton");
+        private ILocator resetPasswordPopUpCloseButton => _page.Locator("css=button._pendo-close-guide");
         private ILocator email => _page.Locator("#email");
         private ILocator password => _page.Locator("#password");
         private ILocator loginButton => _page.Locator("#next");
@@ -19,6 +21,8 @@
 
         public async Task Login(TestUser testUser)
         {
+            await securityPopUpNextButton.ClickAsync();
+            await resetPasswordPopUpCloseButton.ClickAsync();
             await email.FillAsync(testUser.Email);
             await password.FillAsync(testUser.Password);
             await loginButton.ClickAsync();
